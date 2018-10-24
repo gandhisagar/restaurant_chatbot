b665fdf597f7a368e87732cf53286e030fa6a8cc
Sagar Gandhi <sagarg@sagarg-a01.vmware.com>
2018-10-24 12:54:11 +0530

    Starting point for Restaurant Search Bot

---
+++ settings.py
@@ -0,0 +1,20 @@
+"""
+All Constants that are needed
+"""
+
+# List of Tier 1 and 2 cities for comparison
+TIER_1 = ['Ahmedabad', 'Bangalore', 'Chennai', 'Delhi', 'Hyderabad', 'Kolkata', 'Mumbai', 'Pune']
+TIER_2 = ['Agra', 'Ajmer', 'Aligarh', 'Allahabad', 'Amravati', 'Amritsar', 'Asansol', 'Aurangabad', 'Bareilly',
+          'Belgaum', 'Bhavnagar', 'Bhiwandi', 'Bhopal', 'Bhubaneswar', 'Bikaner', 'Bokaro Steel City',
+          'Chandigarh', 'Coimbatore', 'Cuttack', 'Dehradun', 'Dhanbad', 'Durg-Bhilai Nagar', 'Durgapur',
+          'Erode', 'Faridabad', 'Firozabad', 'Ghaziabad', 'Gorakhpur',
+          'Gulbarga', 'Guntur', 'Gurgaon', 'Guwahati', 'Hubli-Dharwad',
+          'Indore', 'Jabalpur', 'Jaipur', 'Jalandhar', 'Jammu', 'Jamnagar',
+          'Jamshedpur', 'Jhansi', 'Jodhpur', 'Kannur', 'Kanpur', 'Kakinada',
+          'Kochi', 'Kottayam', 'Kolhapur', 'Kollam', 'Kota', 'Kozhikode', 'Kurnool',
+          'Lucknow', 'Ludhiana', 'Madurai', 'Malappuram', 'Mathura', 'Goa', 'Mangalore',
+          'Meerut', 'Moradabad', 'Mysore', 'Nagpur', 'Nanded', 'Nashik', 'Nellore', 'Noida',
+          'Palakkad', 'Patna', 'Pondicherry', 'Raipur', 'Rajkot', 'Rajahmundry', 'Ranchi', 'Rourkela',
+          'Salem', 'Sangli', 'Siliguri', 'Solapur', 'Srinagar', 'Sultanpur', 'Surat', 'Thiruvananthapuram',
+          'Thrissur', 'Tiruchirappalli', 'Tirunelveli', 'Tiruppur', 'Ujjain', 'Vijayapura', 'Vadodara', 'Varanasi',
+          'Vasai-Virar City', 'Vijayawada', 'Visakhapatnam', 'Warangal']
